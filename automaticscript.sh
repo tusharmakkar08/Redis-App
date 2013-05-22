fed4a6af18c29a2509f7fed79545152243ac7484
tusharmakkar08 <tusharmakkar08@gmail.com>
2013-05-22 16:00:43 +0530

    Comments added

--- automaticscript.sh
+++ automaticscript.sh
@@ -9,3 +9,4 @@ insert into student (id,name,subject,marks) values \
 (214,"John Kimberly","Unix Programming",9),\
 (213,"Tushar Makkar","Microprocessor Programming",9),\
 (215,"Tim Berkeley","Data Structures",8)'
+ # Creating database ,  new username and Password and  Inserting Entries into the table
